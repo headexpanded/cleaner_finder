0eb25641e7ffeae38d23dd73665672a948bd4123
Mark Hatherly <msfhatherly@gmail.com>
2026-07-23 15:40:54 +1000

    add TogglePassword component

---
+++ src/components/TogglePassword.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-icon
+    class="cursor-pointer"
+    :name="modelValue ? 'visibility' : 'visibility_off'"
+    @click="toggle"
+  />
+</template>
+
+<script setup lang="ts">
+const modelValue = defineModel({
+  type: Boolean,
+  required: true,
+});
+
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean];
+}>();
+
+const toggle = () => {
+  emit('update:modelValue', !modelValue.value);
+};
+</script>
+
+<style scoped lang="scss"></style>
